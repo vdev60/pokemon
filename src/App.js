b9fd9f82529d7d8d3fbd54e0938aa673d7516425
vdev60 <dved490@gmail.com>
2021-11-09 13:48:05 +0200

    Added graphql client

--- src/App.js
+++ src/App.js
@@ -4,24 +4,36 @@ import Home from "./components/Pages/Home";
 import { Container } from "@mui/material";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pokemons from "./components/Pokemons/Pokemons";
+import Ricks from "./components/Ricks/Ricks";
+import { ApolloClient, ApolloProvider,InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <React.StrictMode>
-        <Navbar />
-        <Container sx={{ mt: 5 }}>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/pokemons">
-              <Pokemons />
-            </Route>
-          </Switch>
-        </Container>
-      </React.StrictMode>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <React.StrictMode>
+          <Navbar />
+          <Container sx={{ mt: 5 }}>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/pokemons">
+                <Pokemons />
+              </Route>
+              <Route path="/rick-and-morty">
+                <Ricks />
+              </Route>
+            </Switch>
+          </Container>
+        </React.StrictMode>
+      </Router>
+    </ApolloProvider>
   );
 }
 
